de1c8c3527a4134defa074dfbdc39163b7c82c16
Jeferson <jefersonfguimaraes@gmail.com>
2021-04-07 00:05:56 -0300

    Input file reading

--- sequence_aligner.py
+++ sequence_aligner.py
@@ -2,6 +2,34 @@ import argparse
 import sys
 
 
+class Scores:
+    def __init__(self, gap, match, mismatch):
+        self.gap = gap
+        self.match = match
+        self.mismatch = mismatch
+
+
+class SequenceDefinition:
+    def __init__(self, identifier, description, sequence):
+        self.identifier = identifier
+        self.description = description
+        self.sequence = sequence
+
+
+def read_input_file(input_file_path):
+    read_sequence_list = []
+
+    with open(input_file_path) as file:
+        file_contents = file.readlines()
+
+    read_scores = Scores(int(file_contents[0]), int(file_contents[1]), int(file_contents[2]))
+
+    for i in range(3, len(file_contents), 3):
+        read_sequence_list.append(SequenceDefinition(file_contents[i], file_contents[i+1], file_contents[i+2]))
+
+    return read_scores, read_sequence_list
+
+
 # Main script
 parser = argparse.ArgumentParser(description="Use this tool to execute the desired algorithm for sequence alignment "
                                              "on the sequences informed on the input file")
@@ -13,7 +41,8 @@ parser.add_argument("input_file", help="Path to the file containing the scores f
 args = parser.parse_args()
 
 if args.algorithm == "global":
-    raise Exception("Method not implemented")
+    scores, sequence_list = read_input_file(args.input_file)
+    raise Exception("Method not fully implemented")
 elif args.algorithm == "local":
     raise Exception("Method not implemented")
 else:
